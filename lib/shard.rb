10698c4dfd82932b255148a39590d0f766bee465
Mark Josef <McPhage@gmail.com>
2013-10-09 00:52:51 -0400

    Implementation of CLI commands
    The basics now work!

--- lib/shard.rb
+++ lib/shard.rb
@@ -13,16 +13,22 @@ require 'shard/cli'
 
 class Shard
 
+  class << self
+    attr_reader :current_loader
+  end
+
 end
 
 module Kernel
 
   def shard(shard_line)
-
+    Shard::Loader.load(shard_line)
   end
 
   def require_shard_file(filename)
-    
+    raise ArgumentError unless Shard.current_loader
+
+    Shard.current_loader.load_file(filename)
   end
 
 end
--- lib/shard/cli.rb
+++ lib/shard/cli.rb
@@ -2,6 +2,12 @@ require 'thor'
 
 class Shard::CLI < Thor
 
+  ########
+  #      #
+  # List #
+  #      #
+  ########
+  
   desc "list USERNAME", "List all shards for Github user USERNAME"
   
   def list(username)
@@ -22,6 +28,44 @@ class Shard::CLI < Thor
       puts "Github user #{ username } does not have any shards."
       puts "Shards are gists that have a file named shard.rb or <foo>.shard.rb"
     end
+  end
+
+  ########
+  #      #
+  # Exec #
+  #      #
+  ########
+  
+  desc "exec USERNAME SHARD", "Runs the shard named SHARD for Github user USERNAME"
+
+  def exec(username, shard)
+    Shard::Loader.load "#{ username }/#{ shard }"
+  end
+
+  desc "fetch USERNAME SHARD", "Downloads the shard named SHARD for Github user USERNAME"
+
+  #########
+  #       #
+  # Fetch #
+  #       #
+  #########
+  
+  def fetch(username, shard)
+    shard = Shard::ShardRecord.new(username, shard, 'HEAD')
+
+    Shard::Saver.save(shard)
+  end
+
+  ########
+  #      #
+  # Test #
+  #      #
+  ########
+  
+  desc "test USERNAME SHARD", "Runs the tests for shard named SHARD for Github user USERNAME"
+
+  def test(username, shard)
+    Shard::Loader.test "#{ username }/#{ shard }"
   end
 
 end
