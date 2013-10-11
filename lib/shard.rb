7f4f7daa811683c9b31720fa92fad6c4386cf34f
Mark Josef <McPhage@gmail.com>
2013-10-10 22:12:40 -0400

    Moved the requires into the Shard module
    It doesn't do anything, but maybe look nicer

--- lib/shard.rb
+++ lib/shard.rb
@@ -5,12 +5,13 @@ class Shard
 
   SHARD_LINE = %r{([a-zA-Z0-9_-]+)/(\w+)(?::(\w+))?}
 
+  require 'shard/shard_directory'
+  require 'shard/loader'
+  require 'shard/saver'
+  require 'shard/lister'
+  require 'shard/gist'
+  require 'shard/shard_record'
+  require 'shard/kernel'
+
 end
 
-require 'shard/shard_directory'
-require 'shard/loader'
-require 'shard/saver'
-require 'shard/lister'
-require 'shard/gist'
-require 'shard/shard_record'
-require 'shard/kernel'
